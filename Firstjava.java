35f8bb1bfe1d49fff93320c43ff2559ed13bb651
nitishguptagithub <104243015+nitishguptagithub@users.noreply.github.com>
2023-04-06 21:55:01 +0530

    upload

---
+++ Firstjava.java
@@ -0,0 +1,9 @@
+package Nitishh;
+
+public class Firstjava {
+	public static void main(String args[])
+	{
+		System.out.println("Ntish gupta");
+	}
+
+}
---
+++ Pawan.java
@@ -0,0 +1,10 @@
+package Nitishh;
+
+public class Pawan {
+//	System.out.println("Ntish gupta");
+	public static void main(String args[])
+	{
+		System.out.println("pawan gupta");
+	}
+
+}
